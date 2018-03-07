5048639d4755f695dfdf19dd9c69058ddf702be8
Jacob Grubb <jgrubb013@gmail.com>
2018-03-07 00:06:36 -0600

    Added project requirements elicitation, as well as basic class structure.

--- Project4/Driver.cs
+++ Project4/Driver.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Project4
 {
-    class Program
+    class Driver
     {
         static void Main(string[] args)
         {
