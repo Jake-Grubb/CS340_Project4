6d5840d80229d3bf6df542b93c37b13eb0898774
Jacob Grubb <jgrubb013@gmail.com>
2018-03-08 19:23:46 -0600

    Added additional functionality to Edge class, as well as added functionality to Node class.

--- Project4/Edge.cs
+++ Project4/Edge.cs
@@ -6,15 +6,37 @@ namespace Project4
 {
     class Edge
     {
-        int weight;
-        Node startNode;
-        Node endNode;
+        public static List<Edge> edgeList = new List<Edge>();
+        private int weight;
+        private Node startNode;
+        private Node endNode;
         
+        public static void swapNodesInList(int entry1, int entry2)
+        {
+            Edge temp = edgeList[entry1];
+            edgeList[entry1] = edgeList[entry2];
+            edgeList[entry2] = temp;
+        }
         public Edge(Node newStart, Node newEnd, int newWeight)
         {
             this.startNode = newStart;
             this.endNode = newEnd;
             this.weight = newWeight;
+            edgeList.Add(this);
+        }
+
+        public int getWeight()
+        {
+            return this.weight;
+        }
+
+        public Node getStartNode()
+        {
+            return this.startNode;
+        }
+        public Node getEndNode()
+        {
+            return this.endNode;
         }
 
         public static bool operator <(Edge edge1, Edge edge2)
@@ -51,6 +73,7 @@ namespace Project4
         {
             return (this.weight == edge1.weight) && (this.startNode == edge1.endNode) && (this.endNode == edge1.endNode);
         }
+
         public override int GetHashCode()
         {
             var hashCode = 560889254;
--- Project4/Node.cs
+++ Project4/Node.cs
@@ -6,6 +6,19 @@ namespace Project4
 {
     class Node
     {
-        Edge[] edgeList;
+        public static List<Node> nodeList = new List<Node>();
+        private List<Edge> myEdgeList = new List<Edge>();
+        private string title;
+
+        public Node(string newTitle)
+        {
+            this.title = newTitle;
+            nodeList.Add(this);
+        }
+
+        public void addEdge(Node targetNode, int weight)
+        {
+            this.myEdgeList.Add(new Edge(this, targetNode, weight));
+        }
     }
 }
